04eb750d9daef9c2d3abe0e1e216e5db78f11329
Gustavo Dias A <gfdiasa@gmail.com>
2020-09-24 22:00:22 -0300

    Add sqlalchemy

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
-
+from sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
 
 
 from routes import *
