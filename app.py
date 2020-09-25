974fecca62315a6b3644cedb847857a200c2a8dc
Gustavo Dias A <gfdiasa@gmail.com>
2020-09-24 22:27:22 -0300

    Create database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key'
